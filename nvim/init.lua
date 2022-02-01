cfe08b69e182c4da20688c4e0a5fd7c0de3e55f7
Sergey-Voskresenskiy <work.sergey.voskresenskiy@gmail.com>
2022-02-01 17:23:12 +0200

    more base settings for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,23 @@
 vim.wo.number = true
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
+vim.opt.fillchars = { vert = ' ' }
+vim.opt.showtabline = 1
+vim.opt.scrolloff = 5
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+vim.opt.hidden = true
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.encoding = 'UTF-8'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.tabstop = 2
+vim.opt.spelllang = 'it'
+vim.opt.softtabstop = 2
+vim.opt.swapfile = false
+vim.opt.undofile = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 
 -- packer
 require('plugins')
